950cfaf8824dcf8ea67625bfad7d33f4c3fe9890
13addata <jianxing17000@outlook.com>
2018-09-27 18:21:52 -0400

    All shortcuts is now rendering by vue

--- js/main.js
+++ js/main.js
@@ -6,19 +6,73 @@ var app = new Vue({
 		link: '',
 		showPanel: false,
 		delseen: false,
-		group4: false,
-		group5: true,
 		list: [
+		{
+			name: 'Google',
+			link: 'https://www.google.com/',
+			icon: 'https://www.google.com/favicon.ico',
+			group: 'Favorite'
+		},
+		{
+			name: 'Baidu',
+			link: 'https://www.baidu.com/',
+			icon: 'https://www.baidu.com/favicon.ico',
+			group: 'Favorite'
+		},
+		{
+			name: 'Facebook',
+			link: 'https://facebook.com/',
+			icon: 'https://facebook.com/favicon.ico',
+			group: 'Social'
+		},
+		{
+			name: 'Twitter',
+			link: 'https://twitter.com/',
+			icon: 'https://twitter.com/favicon.ico',
+			group: 'Social'
+		},
+		{
+			name: 'Instagram',
+			link: 'https://www.Instagram.com/',
+			icon: 'https://www.Instagram.com/favicon.ico',
+			group: 'Social'
+		},
 		{
 			name: '博客园',
 			link: 'https://www.cnblogs.com/',
-			icon: 'https://www.cnblogs.com/favicon.ico'
+			icon: 'https://www.cnblogs.com/favicon.ico',
+			group: 'Academy'
 		},
 		{
 			name: 'Segmentfault',
-			link: 'https://segmentfault.com/',
-			icon: 'https://segmentfault.com/favicon.ico'
-		}
+			link: 'https://www.segmentfault.com/',
+			icon: 'https://www.segmentfault.com/favicon.ico',
+			group: 'Academy'
+		},
+		{
+			name: 'Vue.js',
+			link: 'https://vuejs.org/v2/guide/',
+			icon: 'https://vuejsexamples.com/favicon.ico',
+			group: 'Academy'
+		},
+		{
+			name: 'New York Times',
+			link: 'https://www.nytimes.com/',
+			icon: 'https://www.nytimes.com/favicon.ico',
+			group: 'News'
+		},
+		{
+			name: 'Youtube',
+			link: 'https://www.youtube.com/',
+			icon: 'https://www.youtube.com/favicon.ico',
+			group: 'Entertainment'
+		},
+		{
+			name: 'netflix',
+			link: 'https://www.netflix.com/',
+			icon: 'https://www.netflix.com/favicon.ico',
+			group: 'Entertainment'
+		},
 		]
 	},
 	methods : {
